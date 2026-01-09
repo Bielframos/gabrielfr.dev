6d5a52cd2254acc24a2d9fd16ba73773a0b80a02
Gabriel França <gabrielfrancaramos99@gmail.com>
2026-01-09 08:59:53 -0300

    feat: create revalidate api route

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,17 @@
+import { revalidateTag } from "next/cache"
+import { type NextRequest, NextResponse } from "next/server"
+
+export async function GET(request: NextRequest) {
+  const tags = request.nextUrl.searchParams.getAll("tag")
+
+  if (tags.length >= 1) {
+    for (const tag of tags) {
+      revalidateTag(tag, 'max')
+    }
+
+    return NextResponse.json({ revalidated: true, now: Date.now() })
+  }
+
+  return new Response("Use a valid tag", { status: 400 })
+}
+
